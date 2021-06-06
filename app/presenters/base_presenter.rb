9a3fbc3dff085398c1affb06f38106a7794b21be
devHaitham481 <devHaitham481@gmail.com>
2021-06-06 09:12:34 +0300

    Create Presenters

--- app/presenters/base_presenter.rb
+++ app/presenters/base_presenter.rb
@@ -1,4 +1,20 @@
 class BasePresenter 
+    @build_attributes = []
+
+    # Opent the door to class methods 
+ 
+
+  class << self 
+    attr_accessor :build_attributes 
+
+    # Create the actual class method that will \
+    # be used in the subclasses
+    # we use the splash operation '*' to get all
+    # the arguments passes in an array
+    def build_with(*args)
+      @build_attributes = args.map(&:to_s)
+    end
+  end
     attr_accessor :object, :params, :data
 
     def initialize(object, params, options = {} )
